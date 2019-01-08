2143bc11cdc3372b7213d92c37d4886e4aee7b18
zhongyuqiang <z_haloword@163.com>
2019-01-08 19:51:14 +0800

    '轨迹'

--- src/views/behavior/trail.vue
+++ src/views/behavior/trail.vue
@@ -50,7 +50,9 @@
           </div>
         </el-scrollbar>
       </div>
-      <div class="td--right dashed--border"></div>
+      <div class="td--right dashed--border">
+        <trail-map></trail-map>
+      </div>
     </div>
   </div>
 </template>
@@ -58,9 +60,13 @@
 <script>
 import {parseTime} from '../../utils'
 import {GetPersonTrail} from '../../api/behavior'
+import TrailMap from '../../components/three/trail_map'
 
 export default {
   name: 'trail',
+  components: {
+    TrailMap
+  },
   data () {
     return {
       spoorDate: '',
