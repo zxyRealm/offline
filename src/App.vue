4c8343257226ef776b65c868381281e4f8df4e1b
折威 <zhangxiaoyuan@uni-ubi.com>
2019-04-23 15:24:31 +0800

    fix: 定义默认路由为登录页
    * chore: 去除App.vue中废弃的全局接口请求

--- src/App.vue
+++ src/App.vue
@@ -13,17 +13,17 @@ export default {
   name: 'App',
   created () {
     console.log('version 2018-04-12  ' + version)
-    GetSocketIP().then(res => {
-      this.$store.state.serverIp = `${res.data || ''}`
-    })
-    this.$store.dispatch('GET_USER_INFO')
-    if (!this.userInfo.company) {
-      GetMerchantInfo().then(res => {
-        this.$store.commit('SET_USER_INFO', res.data)
-      }).catch(err => {
-        console.log(err)
-      })
-    }
+    // GetSocketIP().then(res => {
+    //   this.$store.state.serverIp = `${res.data || ''}`
+    // })
+    // this.$store.dispatch('GET_USER_INFO')
+    // if (!this.userInfo.company) {
+    //   GetMerchantInfo().then(res => {
+    //     this.$store.commit('SET_USER_INFO', res.data)
+    //   }).catch(err => {
+    //     console.log(err)
+    //   })
+    // }
   },
   computed: {
     userInfo: function () {
