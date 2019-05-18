066df82cc164439cb247c146101450947336de82
zhongxiaoyan <zhongxiaoyan@uni-ubi.com>
2019-05-18 14:01:48 +0800

    feat(人员+开发者):删除多余部分

--- src/api/member.js
+++ src/api/member.js
@@ -1,144 +1,5 @@
 import request from '@/utils/request'
 import prefix from './prefix'
-
-// 会员库信息修改
-export function MemberLibraryUpdate (data) {
-  return request({
-    url: '/memberLibrary/update',
-    data
-  })
-}
-
-// 会员是否已存在
-export function MemberExistPhone (data) {
-  return request({
-    tip: false,
-    url: '/member/exist/phone',
-    data
-  })
-}
-
-// 会员库创建
-export function MemberLibraryCreate (data) {
-  return request({
-    url: '/memberLibrary/create',
-    data
-  })
-}
-
-// 删除会员库信息
-export function MemberLibraryDelete (data) {
-  return request({
-    url: '/memberLibrary/delete',
-    data
-  })
-}
-
-// 管理社群下会员库列表
-export function MemberLibraryList (data) {
-  return request({
-    url: '/memberLibrary/list',
-    data
-  })
-}
-
-// 会员库信息查找
-export function MemberLibraryFind (data) {
-  return request({
-    url: '/memberLibrary/find',
-    data
-  })
-}
-
-// 会员库名是否重复
-export function MemberLibraryNameExist (data) {
-  return request({
-    tip: false,
-    url: '/memberLibrary/name/exist',
-    data
-  })
-}
-
-// 会员库信息查询、获取
-export function MemberLibrarySearch (data) {
-  return request({
-    url: '/memberLibrary/search',
-    data
-  })
-}
-
-// 获取问题列表
-export function MemberTypeList (data) {
-  return request({
-    url: '/memberType/list',
-    data
-  })
-}
-
-// 获取问题更新
-export function MemberTypeOperate (data) {
-  return request({
-    url: '/memberType/operate',
-    data
-  })
-}
-
-// 人员信息更新
-export function MemberCreate (data) {
-  return request({
-    url: '/member/create',
-    data
-  })
-}
-
-// 人员信息更新
-export function MemberUpdate (data) {
-  return request({
-    url: '/member/update',
-    data
-  })
-}
-
-// 人员信息删除
-export function MemberDelete (data) {
-  return request({
-    url: '/member/del',
-    data
-  })
-}
-
-// 人员列表
-export function MemberSearch (data) {
-  return request({
-    url: '/member/search',
-    data
-  })
-}
-
-// 人员列表
-export function MemberTemplate (data) {
-  return request({
-    url: '/member/template',
-    data
-  })
-}
-
-// 人员信息获取
-export function MemberDetails (data) {
-  return request({
-    url: '/member/details',
-    data
-  })
-}
-
-// 人员图片信息入库
-export function MemberImgDetect (data) {
-  return request({
-    url: '/member/img/detect',
-    data
-  })
-}
-
 /**
 * 线下浏览器1.3
 * */
@@ -153,7 +14,7 @@ export function createPersonLibrary (data) {
 // 删除人员库
 export function deletePersonLibrary (data) {
   return request({
-    url: `${prefix[3]}/personLibrary/delete`,
+    url: `${prefix[3]}/personLibrary/delete/${data.personLibraryId}`,
     method: 'DELETE',
     data
   })
@@ -161,7 +22,7 @@ export function deletePersonLibrary (data) {
 // 单个人员库信息查询
 export function getPersonLibraryById (data) {
   return request({
-    url: `${prefix[3]}/personLibrary/select`,
+    url: `${prefix[3]}/personLibrary/select/${data.personLibraryId}`,
     method: 'POST',
     data
   })
@@ -193,7 +54,7 @@ export function createPerson (data) {
 // 删除人员
 export function deletePerson (data) {
   return request({
-    url: `${prefix[3]}/person/delete`,
+    url: `${prefix[3]}/person/delete/${data.personId}`,
     method: 'DELETE',
     data
   })
@@ -257,7 +118,7 @@ export function updatePersonType (data) {
 // 人员类型删除接口
 export function deletePersonType (data) {
   return request({
-    url: `${prefix[3]}/personType/delete`,
+    url: `${prefix[3]}/personType/delete/${data.id}`,
     method: 'DELETE',
     data
   })
